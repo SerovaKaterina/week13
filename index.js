74a931d514209bf400269b5cbf7128d3227f705b
SerovaKaterina <k.serova.k@yandex.ru>
2023-06-05 02:34:20 +0300

    add name function change

--- index.js
+++ index.js
@@ -6,11 +6,24 @@ const btn = document.querySelector(".form__btn");
 const resultName = document.querySelector(".fullname");
 const resultComment = document.querySelector(".yourcomment");
 
-function changeName() {
+/*  function changeName() {
   let capital = fullName.value.charAt(0).toUpperCase();
   let rest = fullName.value.slice(1).toLowerCase();
   resultName.innerHTML = capital + rest;
+}*/
+function changeName() {
+  let checkbox = document.querySelector('input[name="check"]:checked').value;
+  if ((checkbox == "Yes") && (fullName != "")) {
+      let capital = fullName.value.charAt(0).toUpperCase();
+      let rest = fullName.value.slice(1).toLowerCase();
+      resultName.innerHTML = capital + rest;
+  }
+  else if (checkbox == "No") {
+      resultName.textContent = 'Username';
+  }
 }
+
+
 function changeComment() {
   resultComment.innerHTML = yourComment.value.replace(/(xxx|viagra)/gi, "***");
 }
